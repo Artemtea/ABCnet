bc4509ae62799b12fa1ce2ea67346e225699c003
Artemtea <96112762+Artemtea@users.noreply.github.com>
2022-10-13 22:37:06 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,24 @@
+﻿var 
+  a:array [1..10] of integer;
+  k,max: integer;
+begin
+  for var i:=1 to 10 do
+  begin
+     a[i]:= random(21);
+     write(a[i],' ');
+  end;
+  writeln;
+  max:=a[1]; 
+  k:=1;
+  for var i:=2 to 10 do
+  begin  
+    if a[i] > max then
+    begin
+      max:= a[i];
+      k:=1;
+    end;
+  end;
+  writeln('макс эл. =  ', max );
+  writeln('номер эл.  ', k );
+
+end.
